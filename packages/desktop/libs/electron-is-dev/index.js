daf2b2d131fa29e1f1aff5717a01b733bea99f82
bartekus <bartekus@gmail.com>
2019-04-27 21:01:52 -0700

    Add desktop electron CRA wrapper functionality

---
+++ packages/desktop/libs/electron-is-dev/index.js
@@ -0,0 +1,11 @@
+// Source: https://github.com/sindresorhus/electron-is-dev
+
+'use strict';
+const electron = require('electron');
+
+const app = electron.app || electron.remote.app;
+
+const isEnvSet = 'ELECTRON_IS_DEV' in process.env;
+const getFromEnv = parseInt(process.env.ELECTRON_IS_DEV, 10) === 1;
+
+module.exports = isEnvSet ? getFromEnv : !app.isPackaged;
